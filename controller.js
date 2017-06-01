eed4313b4450363665d1dc7a4df6c9ffbe585fb6
Anton Tanderup <antontandrup@gmail.com>
2017-06-01 14:51:24 +0200

    intro and outro added

--- controller.js
+++ controller.js
@@ -1,7 +1,9 @@
 var rootFolder = document.location.pathname;
 
+$(".shower-start-blur").hide();
+
 var updateTimeline = function () {
-  chakraSelection.time = 0;
+  chakraSelection.time = 1 + .32;
   $("#time-line").empty();
   for (i = 0; i < chakraSelection.playbackOrder.length; ++i) {
 
@@ -16,16 +18,21 @@ var updateTimeline = function () {
 }
 
 var updateTime = function () {
-  $("#time-output").html(chakraSelection.time + " min.");
+  $("#time-output").html(chakraSelection.time.toFixed(2) + " min.");
       //<input id="time-line-range" name="time-line-range" type ="range" min="-2.5" max="3.0" step ="0.1"/>
 }
 
 var playShower = function() {
-  var audio = new Audio(rootFolder + 'audio/chakra_' + chakraSelection.playbackOrder[0] + '.wav');
-  var audioFreq = new Audio(rootFolder + 'audio/chakra_' + chakraSelection.playbackOrder[0] + '_frequency.wav');
-  audio.play(); audioFreq.play();
+  $(".shower-start-blur").show();
+  var intro =  new Audio(rootFolder + 'audio/intro.wav');
+  intro.play();
+  setTimeout(function(){
+    var audio = new Audio(rootFolder + 'audio/chakra_' + chakraSelection.playbackOrder[0] + '.wav');
+    var audioFreq = new Audio(rootFolder + 'audio/chakra_' + chakraSelection.playbackOrder[0] + '_frequency.wav');
+    audio.play(); audioFreq.play();
+  }, 60000);
   for (i = 1; i < chakraSelection.playbackOrder.length; ++i) {
-    var delay = i * 120000;
+    var delay = (i * 120000) + 60000;
     var audioFile = rootFolder + 'audio/chakra_' + chakraSelection.playbackOrder[i] + '.wav';
     var audioFreqFile = rootFolder + 'audio/chakra_' + chakraSelection.playbackOrder[i] + '_frequency.wav';
     setTimeout(function(audioFile){
@@ -34,6 +41,11 @@ var playShower = function() {
       audio.play(); audioFreq.play();
     }, delay, audioFile);
   }
+  var outroDelay = (chakraSelection.playbackOrder.length * 120000) + 60000;
+  setTimeout(function(){
+    var outro = new Audio(rootFolder + 'audio/outro.wav');
+    outro.play();
+  }, outroDelay);
 
 }
 
